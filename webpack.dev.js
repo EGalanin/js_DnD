1d8e37370c064a39d3dab37aa34dab6f1dc3158d
Sanysh <galanin09234@mail.ru>
2023-11-08 15:41:03 +0300

    changed webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,22 +1,10 @@
-const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.config');
 
 module.exports = merge(common, {
-  // Set the mode to development or production
   mode: 'development',
-  // Control how source maps are generated
-  devtool: 'inline-source-map',
-
-  // Spin up a server for quick development
   devServer: {
-    historyApiFallback: true,
-    open: true,
-    compress: true,
+    port: 9000,
   },
-
-  plugins: [
-    // Only update what has changed on hot reload
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+  devtool: 'source-map',
 });
